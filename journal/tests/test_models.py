8921ee26287cb61b1a701b22e4840750a549ef51
Damien <damienreganmusic@gmail.com>
2025-09-15 21:51:24 +0100

    Add tests directory

--- journal/tests/test_models.py
+++ journal/tests/test_models.py
@@ -4,3 +4,13 @@ from ..models import DescentType, DescentSession, Entry
 
 User = get_user_model()
 
+class TestDescentType(TestCase):
+    def test_create_descent_type(self):
+        """ Test creating a new descent type """
+        descent_type = DescentType.objects.create(
+            name="Test Descent",
+            description="A test descent type"
+        )
+        self.assertEqual(str(descent_type), "Test Descent")
+        self.assertEqual(descent_type.description, "A test descent type")
+        self.assertEqual(descent_type.is_active)
