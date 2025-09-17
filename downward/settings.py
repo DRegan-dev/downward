4ad68a7058cde7e482f3cf7093a8a93b2172406b
Damien <damienreganmusic@gmail.com>
2025-09-17 11:05:12 +0100

    Debug set for deployment

--- downward/settings.py
+++ downward/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-**0lkm!+!k!r%i=_-nq53y=uh4i4=-w@%d&+*p-nkd)kfh(a5z'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 if not SECRET_KEY:
@@ -31,7 +30,7 @@ if not SECRET_KEY:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.devtunnels.ms', 'down-ward-ad9d86ecbdd0.herokuapp.com']
 
 
